ea381c746fe905eeeec06c24b180731edf2aa000
root <root@vagrant.vm>
2023-11-25 22:07:10 +0800

    adv paramter error message

---
+++ adv_var/test_host.with.help.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+
+	###################################################
+
+	TEST_HOST=${1?"Usage: $0 <IP>"}
+  OTHER_HOST=${2?"Usage: xxxx"}
+	# ping -c4 www.163.com  1> /dev/null 2> /dev/null 
+	#ping -c4 $TEST_HOST  &> /dev/null
+	#echo $?
+        if ping -c4 $TEST_HOST &> /dev/null ;then
+          	echo -e "test $TEST_HOST is \033[32mSucceed\033[m"
+        else
+          	echo -e "test $TEST_HOST is \033[31mFailed\033[m"
+        fi
+
+
+
+
+
+
