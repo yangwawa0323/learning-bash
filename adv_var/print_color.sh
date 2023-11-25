310e1d7ef23add3851eb3c5023f7917854a6e5ff
root <root@vagrant.vm>
2023-11-25 22:07:10 +0800

    adv bash array

---
+++ adv_var/print_color.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+COLORS=('purple' 'reddish-orange' 'light green')
+
+
+echo "${COLORS[@]}"
+for color in "${COLORS[@]}"
+do
+		echo $color
+
+done
+
+#echo "${COLORS[0]}"
+#echo "${COLORS[1]}"
+#echo "${COLORS[2]}"
